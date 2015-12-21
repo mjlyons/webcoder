a3173484154316e3e33051ab74eb424a08314a0c
Michael J. Lyons <github@explosivedonut.com>
2015-12-20 19:19:36 -0500

    Minor tmux tweak

--- webcoder-server/dev-tmux.sh
+++ webcoder-server/dev-tmux.sh
@@ -3,5 +3,6 @@
 tmux new-session -d 'npm run watch:build'
 tmux split-window -v 'npm run watch:test'
 tmux split-window -v 'npm run watch:start'
+tmux split-window -v
 tmux select-layout even-vertical
 tmux -2 attach-session -d
