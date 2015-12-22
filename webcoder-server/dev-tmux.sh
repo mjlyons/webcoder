8c8f914a8131f96a4c5e4a192d46730ee86686e1
Michael J. Lyons <github@explosivedonut.com>
2015-12-22 00:41:29 -0500

    Removing dev-tmux.sh - npm watch:start works better

--- webcoder-server/dev-tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d 'npm run watch:build'
-tmux split-window -v 'npm run watch:test'
-tmux split-window -v 'npm run watch:start'
-tmux split-window -v
-tmux select-layout even-vertical
-tmux set-window-option -g mode-mouse on
-tmux -2 attach-session -d
