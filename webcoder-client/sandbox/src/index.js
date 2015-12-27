671304eac359d176407848ace87808d033a10ea3
Michael J. Lyons <buzz.github@clearhive.com>
2015-12-27 09:48:35 -0600

    Starting the front-end for file nav

---
+++ webcoder-client/sandbox/src/index.js
@@ -0,0 +1,32 @@
+const FolderBrowserEntry = React.createClass({
+  propTypes: {
+  //  iconClasses: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+    classnames: React.PropTypes.string.isRequired,
+    filename: React.PropTypes.string.isRequired,
+    id: React.PropTypes.string.isRequired,
+  },
+  render: function() {
+    return (
+      <div className="folder-browser-entry">
+        <span className="file-icon"><i className={this.props.classnames}></i></span>
+        <span className="file-name">{this.props.filename}</span>
+      </div>
+    );
+  }
+});
+
+const FolderBrowser = React.createClass({
+  render: () => {
+    return (
+      <div>
+        <FolderBrowserEntry classnames="fa fa-folder" filename="myfolder" id="/myfolder" />
+        <FolderBrowserEntry classnames="fa fa-file" filename="myfile.txt" id="/myfile.txt" />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(FolderBrowser, null),
+  document.getElementById('content')
+);
