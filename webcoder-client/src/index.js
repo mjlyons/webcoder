b423021f4e4368a05039a6d85ae9825aff381a99
Michael J. Lyons <buzz.github@clearhive.com>
2015-12-27 19:14:18 -0600

    Not hardcoding each file

--- webcoder-client/src/index.js
+++ webcoder-client/src/index.js
@@ -1,35 +1,59 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const filetypes = {
+  FILE: Symbol(),
+  FOLDER: Symbol(),
+};
+
 const FolderBrowserEntry = React.createClass({
-  propTypes: {
+  //propTypes: {
   //  iconClasses: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
-    classnames: React.PropTypes.string.isRequired,
-    filename: React.PropTypes.string.isRequired,
-    id: React.PropTypes.string.isRequired,
+  //  classnames: React.PropTypes.string.isRequired,
+  //  filename: React.PropTypes.string.isRequired,
+  //  id: React.PropTypes.string.isRequired,
+  //},
+  getClassNamesFromFiletype: function(filetype) {
+    switch (filetype) {
+      case filetypes.FILE:
+        return 'fa fa-file';
+      case filetypes.FOLDER: 
+        return 'fa fa-folder';
+      default:
+        throw new Error(`Unexpected filetype: ${filetype.toString()}`);
+    }
   },
   render: function() {
     return (
       <div className="folder-browser-entry">
-        <span className="file-icon"><i className={this.props.classnames}></i></span>
-        <span className="file-name">{this.props.filename}</span>
+        <span className="file-icon"><i className={this.getClassNamesFromFiletype(this.props.fileinfo.filetype)}></i></span>
+        <span className="file-name">{this.props.fileinfo.filename}</span>
       </div>
     );
   }
 });
 
-const FolderBrowser = React.createClass({
+const FolderBrowserEntryList = React.createClass({
   render: function() {
+    const fileEntries = this.props.folderInfo.map(function(fileInfo) {
+      return (
+        <FolderBrowserEntry fileinfo={fileInfo} />
+      );
+    });
     return (
       <div>
-        <FolderBrowserEntry classnames="fa fa-folder" filename="my-awesome-folder" id="/myfolder" />
-        <FolderBrowserEntry classnames="fa fa-file" filename="myfile.txt" id="/myfile.txt" />
+        {fileEntries}
       </div>
     );
   }
 });
 
+const testData = [
+  { filetype: filetypes.FILE, filename: 'myfile.txt', id: '/myfolder/myfile.txt' },
+  { filetype: filetypes.FILE, filename: 'myfile2.txt', id: '/myfolder/myfile.txt' },
+  { filetype: filetypes.FOLDER, filename: 'mySubFolder', id: '/myfolder/mysubfolder' },
+];
 ReactDOM.render(
-  React.createElement(FolderBrowser, null),
+  React.createElement(FolderBrowserEntryList, { folderInfo: testData }),
   document.getElementById('content')
 );
