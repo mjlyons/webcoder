4e6d981a12357d1d8cbe687188d8b5554d33440a
Michael J. Lyons <buzz.github@clearhive.com>
2015-12-28 18:20:57 -0600

    Add a unit test for the folder brower entry list

---
+++ webcoder-client/js/__tests__/FolderBrowserEntryList.js
@@ -0,0 +1,24 @@
+jest.dontMock('js/FileEntry');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const FolderBrowserEntry = require('js/FolderBrowserEntry');
+const FolderBrowserEntryList = require.requireActual('js/FolderBrowserEntryList');
+const { Filetypes } = require.requireActual('js/FileEntry');
+
+describe('FolderBrowserEntryList', () => {
+  it('displays a list with a file and a folder', () => {
+    const folderContents = [
+      { filetype: Filetypes.FILE, filename: 'myfile.txt', path: '/myfolder/myfile.txt' },
+      { filetype: Filetypes.FOLDER, filename: 'mySubFolder', path: '/myfolder/mysubfolder' },
+    ];
+    const folderBrowserEntryList = TestUtils.renderIntoDocument(
+      <FolderBrowserEntryList folderInfo={folderContents} />
+    );
+    const entryNodes = TestUtils.scryRenderedComponentsWithType(folderBrowserEntryList, FolderBrowserEntry);
+    expect(entryNodes[0].props.fileinfo).toBe(folderContents[0]);
+    expect(entryNodes[1].props.fileinfo).toBe(folderContents[1]);
+  });
+});
