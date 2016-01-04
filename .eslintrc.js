45a5b5451e65686a79dc5db88ba0c3a60d01e09c
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-03 21:15:48 -0500

    Fix a build settings issue, lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react"
-    ],
-    "env": {
-      "jest": true,
-      'jasmine': true,
-    },
-    "rules": {
-      "no-unused-vars": [2, { "argsIgnorePattern": "^_" }],
-      "new-cap": [2, {"capIsNewExceptions": ["Router"]}]
-    }
+  'extends': 'airbnb',
+  'plugins': [
+    'react',
+  ],
+  'env': {
+    'jest': true,
+    'jasmine': true,
+  },
+  'rules': {
+    'no-unused-vars': [2, { 'argsIgnorePattern': '^_' }],
+    'new-cap': [2, { 'capIsNewExceptions': ['Router'] }],
+  },
 };
