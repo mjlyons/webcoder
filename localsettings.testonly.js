ee1a3b47e6debdc4ddec5ae186450ca542cf96a1
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-10 17:23:37 -0500

    Fixing a localsettings imbalance

--- localsettings.testonly.js
+++ localsettings.testonly.js
@@ -1,11 +1,13 @@
-module.exports = {
-  ROOT_SOURCE_PATH: '/example/webcoder',
+module.exports = function localsettings() {
+  return {
+    ROOT_SOURCE_PATH: '/example/webcoder',
 
-  SERVER_HOST: 'https://example.org:3000',
-  CLIENT_HOST: 'https://example.org:3000',
-  CLIENT_JS_BUNDLE_PATH: '/static/client/bundle.js',
-  WEBPACK_DEV_SERVER_HOST: 'http://localhost:8080',
-  WEBPACK_DEV_SERVER_JS_BUNDLE_PATH: '/bundle.js',
+    SERVER_HOST: 'https://example.org:3000',
+    CLIENT_HOST: 'https://example.org:3000',
+    CLIENT_JS_BUNDLE_PATH: '/static/client/bundle.js',
+    WEBPACK_DEV_SERVER_HOST: 'http://localhost:8080',
+    WEBPACK_DEV_SERVER_JS_BUNDLE_PATH: '/bundle.js',
 
-  FONT_AWESOME_URL: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
+    FONT_AWESOME_URL: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
+  };
 };
