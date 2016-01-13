44d3d54f70be0de1bfe24fa5dfae16e8e707805e
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-12 21:21:32 -0500

    Comment out unused server code, add missing coverage exclusion file

--- js/server/routes/users.js
+++ js/server/routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function handleGetUsers(req, res, _next) {
-  res.send('respond with a resource');
-});
+// NOTE(mike): uncomment this to have authenticated requests
+// router.get('/', function handleGetUsers(req, res, _next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
