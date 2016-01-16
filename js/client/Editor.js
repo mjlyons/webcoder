ce2abe242cd962d6d6a347d1c170ff468e861e0e
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-15 22:05:11 -0500

    Adding support for some filetypes, switch to solarized-dark scheme

--- js/client/Editor.js
+++ js/client/Editor.js
@@ -1,9 +1,19 @@
+const Path = require('path');
 const React = require('react');
 const AceEditor = require('react-ace');
 const EditorStore = require('js/client/stores/EditorStore');
 const SourceFileSystemStore = require('js/client/stores/SourceFileSystemStore');
 
 require('client/style/Editor');
+
+// TODO(mike): Should be smarter than this
+require('brace/mode/javascript');
+require('brace/mode/json');
+require('brace/mode/markdown');
+require('brace/mode/plain_text');
+require('brace/mode/yaml');
+
+require('brace/theme/solarized_dark');
 
 function _getStateFromStores() {
   const currentPath = EditorStore.getState().get('currentPath');
@@ -13,6 +23,7 @@ function _getStateFromStores() {
 
 // TODO(mike): Separate the state/render into controller + component
 // TODO(mike): unit test this
+// TODO(mike): This made the js bundle huge. Figure out how to optimize.
 
 /**
  * React controller for text editor
@@ -48,6 +59,19 @@ class Editor extends React.Component {
   }
 
   render() {
+    // TODO(mike): Somehow script this (like localsettings.js) to only use the formats that are wanted
+    const extToMode = {
+      '.js': 'javascript',
+      '.json': 'json',
+      '.md': 'markdown',
+      '.yaml': 'yaml',
+      '.yml': 'yaml',
+    };
+    const ext = this.state.currentPath ? Path.extname(this.state.currentPath.toLowerCase()) : null;
+    const mode = ext in extToMode ? extToMode[ext] : 'plain_text';
+
+    const theme = 'solarized_dark';
+
     return (
       <AceEditor
         value={this.state.fileContents || ''}
@@ -55,6 +79,8 @@ class Editor extends React.Component {
         className="ace-editor"
         height={null}
         width={null}
+        mode={mode}
+        theme={theme}
       />
     );
   }
