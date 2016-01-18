1c0a3dd97da885620f91b94002dd24c17e07fdc3
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-17 22:27:44 -0500

    Make AlertHeader a controller

--- js/client/AlertHeader.js
+++ js/client/AlertHeader.js
@@ -1,16 +1,41 @@
 const React = require('react');
+const AlertStore = require('js/client/stores/AlertStore');
 
 require('client/style/AlertHeader.scss');
 
+function _getStateFromStores() {
+  return {
+    alertMessage: AlertStore.getState().get('message'),
+  };
+}
+
 class AlertHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = _getStateFromStores();
+    this.onStoreChange = this.onStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    AlertStore.addChangeListener(this.onStoreChange);
+  }
+
+  componentWillUnmount() {
+    AlertStore.removeChangeListener(this.onStoreChange);
+  }
+
+  onStoreChange() {
+    this.setState(_getStateFromStores());
+  }
+
   render() {
     // Don't display header if there's no alert message
-    if (!this.props.message) {
+    if (!this.state.alertMessage) {
       return null;
     }
 
     return (
-      <div className="alert-header">{this.props.message}</div>
+      <div className="alert-header">{this.state.alertMessage}</div>
     );
   }
 }
