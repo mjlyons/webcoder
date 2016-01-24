476cbf2db27d6cba9fc1f555620f6b4e395b726e
Mike Lyons <buzz.github@clearhive.com>
2016-01-23 21:13:48 -0500

    Specify hostname server should listen on (and nothing else)

--- localsettings.ci_mode.js
+++ localsettings.ci_mode.js
@@ -1,7 +1,8 @@
 module.exports = {
   ROOT_SOURCE_PATH: '/example/webcoder',
 
-  SERVER_HOST_PORT: '3000',
+  SERVER_PORT: '3000',
+  SERVER_HOSTNAME: 'example.org',
   SERVER_HOST: 'https://example.org:3000',
   CLIENT_HOST: 'https://example.org:3000',
   CLIENT_JS_BUNDLE_PATH: '/static/client/bundle.js',
