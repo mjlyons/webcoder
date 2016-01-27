60c412ad8455707c9e1ee53a4b054d799008f90b
Michael J. Lyons <buzz.github@clearhive.com>
2016-01-27 00:36:19 -0500

    Got coverage working with mochagit diff

--- test/server/serverfs.test.js
+++ test/server/serverfs.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
+//const rewire = require('rewire');
+//const mockery = require('mockery');
 
 const expect = chai.expect;
 chai.use(require('sinon-chai'));
@@ -8,15 +10,24 @@ chai.use(require('sinon-chai'));
 describe('serverfs', () => {
   describe('readfile', () => {
     it('errors when trying to leave root path', () => {
+      debugger;
+      //mockery.enable();
       const resolveStub = sinon.stub().returns('/snuck-out');
       const serverfs = proxyquire('js/server/serverfs', {
         path: { resolve: resolveStub },
       });
+      //const serverfs = rewire('js/server/serverfs');
+      //serverfs.__set__({
+      //  path: { resolve: resolveStub },
+      //});
+      //mockery.registerMock('path', { resolve: resolveStub });
+      //const serverfs = require('js/server/serverfs');
 
       const result = serverfs.readfile('/rootpath', '../subdir');
       expect(resolveStub).to.have.been.calledOnce;
       expect(resolveStub).to.have.been.calledWith('/rootpath', '../subdir');
-      expect(result).to.be.deep.equal({ error: 'not-authorized' });
+      expect(result).to.be.deep.equal({ error: 'not-authorized'});
+      //mockery.disable();
     });
 
     it("errors when the file doesn't exist", () => {
@@ -33,10 +44,10 @@ describe('serverfs', () => {
       expect(result).to.be.deep.equal({ error: 'not-exists' });
     });
 
-    it('errors when trying to load something other than a file', () => {
+    it("errors when trying to load something other than a file", () => {
       const resolveStub = sinon.stub().returns('/rootpath/subdir');
       const existsSyncStub = sinon.stub().returns(true);
-      const statSyncStub = sinon.stub().returns({ isFile: () => { return false; } });
+      const statSyncStub = sinon.stub().returns({ isFile: () => { return false; }});
       const serverfs = proxyquire('js/server/serverfs', {
         path: { resolve: resolveStub },
         fs: { existsSync: existsSyncStub, statSync: statSyncStub },
@@ -48,10 +59,10 @@ describe('serverfs', () => {
       expect(result).to.be.deep.equal({ error: 'not-file' });
     });
 
-    it('returns the contents of a file', () => {
+    it ('returns the contents of a file', () => {
       const resolveStub = sinon.stub().returns('/rootpath/file.txt');
       const existsSyncStub = sinon.stub().returns(true);
-      const statSyncStub = sinon.stub().returns({ isFile: () => { return true; } });
+      const statSyncStub = sinon.stub().returns({ isFile: () => { return true; }});
       const readFileSyncStub = sinon.stub().returns('fake file contents');
       const serverfs = proxyquire('js/server/serverfs', {
         path: { resolve: resolveStub },
